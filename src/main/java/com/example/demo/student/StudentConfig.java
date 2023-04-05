9ec50497e14f3590ad78d1960a6bf7aabf7f5a70
haydenfish <hayden.fish@icloud.com>
2023-04-04 17:46:08 -0700

    Added functionality, registering student

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -16,15 +16,13 @@ public class StudentConfig {
             Student hayden = new Student(
                     "Hayden",
                     "hayden.fish@gmail.com",
-                    LocalDate.of(2004, Month.MAY, 19),
-                    18
+                    LocalDate.of(2004, Month.MAY, 19)
             );
 
             Student stephen = new Student(
                     "Stephen",
                     "stephen.v@gmail.com",
-                    LocalDate.of(2000, Month.APRIL, 8),
-                    18
+                    LocalDate.of(2000, Month.APRIL, 8)
             );
 
             repository.saveAll(
