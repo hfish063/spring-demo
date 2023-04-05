cb62daa6a17475ac6defad97aadd7aeeec1f2116
haydenfish <hayden.fish@icloud.com>
2023-04-04 17:07:08 -0700

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student hayden = new Student(
+                    "Hayden",
+                    "hayden.fish@gmail.com",
+                    LocalDate.of(2004, Month.MAY, 19),
+                    18
+            );
+
+            Student stephen = new Student(
+                    "Stephen",
+                    "stephen.v@gmail.com",
+                    LocalDate.of(2000, Month.APRIL, 8),
+                    18
+            );
+
+            repository.saveAll(
+                    List.of(hayden, stephen)
+            );
+        };
+    }
+}
